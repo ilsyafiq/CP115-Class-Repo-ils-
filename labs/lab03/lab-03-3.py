d92bea45b22159bf01e800ab1d3ac9c8f846575a
ilsyafiq <syfiqiikhwan@gmail.com>
2025-07-22 12:02:53 +0800

    Senario 2

---
+++ labs/lab03/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1000
+gigabyte = kilobyte / 1000000
+terabyte = kilobyte / 1000000000
+petabyte = kilobyte / 1000000000000
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
