ffec2875a98af2a5ee1c85a0be656bce4b425425
ilsyafiq <syfiqiikhwan@gmail.com>
2025-07-22 12:52:39 +0800

    Scenario 7 (Test)

---
+++ labs/lab03/lab-03-8.py
@@ -0,0 +1,9 @@
+principal = float(input())
+rate = float(input())
+time = float(input())
+interest = principal * rate * time
+totalAmount = principal + interest
+monthlyInterest = 0
+print(interest)
+print(totalAmount)
+print(monthlyInterest)
