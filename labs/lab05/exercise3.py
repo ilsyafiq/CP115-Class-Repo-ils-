a30bbd119b0060107ac1d91e22f68e508526c1e1
ilsyafiq <syfiqiikhwan@gmail.com>
2025-08-05 12:48:06 +0800

    Exercise 3 - Change range of random number

--- labs/lab05/exercise3.py
+++ labs/lab05/exercise3.py
@@ -4,8 +4,8 @@ import random
 # Takes the student's class name from the user
 student_class = input("Enter your class'name: ")
 
-# Generates a random number (1 - 100) and displays class information
-random_number = random.randint(1, 100)
+# Generates a random number (1 - 18) and displays class information
+random_number = random.randint(1, 18)
 
 print("Class: " + student_class)
 print("Random number: " + str(random_number))
