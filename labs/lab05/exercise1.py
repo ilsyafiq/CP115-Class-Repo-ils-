48808e29198b1ad45f551b1fe2d55ea1be6a448b
ilsyafiq <syfiqiikhwan@gmail.com>
2025-08-05 11:39:21 +0800

    Exercise 1

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,9 @@
+# Asks the user for their name, age, and course code && Stores this information in appropriately named variables
+student_name = input("Enter your name: ")        # str (string)
+student_age = int(input("Enter your age: "))     # int (integer)
+course_name = input("Enter the course name: ")   # str (string)
+
+# Displays the student information
+print("Student Name: " + student_name)
+print("Age: " + student_age)
+print("Course: " + course_name)
