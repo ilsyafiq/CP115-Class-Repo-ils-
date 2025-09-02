0d571147fa51ebc58e2b9e9fa5d52a29de1fff1c
ilsyafiq <syfiqiikhwan@gmail.com>
2025-09-02 13:14:29 +0800

    exercise3.py ongoing

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -6,32 +6,31 @@ tax_status = input()
 # TODO your code here
 overtime_pay = overtime_hours * 35
 total_income = base_salary + overtime_pay
-epf_deduction = total_income * 0.11
-socso_deduction = total_income * 0.05
-net_salary_before_tax = total_income - epf_deduction - socso_deduction
+epf_deduction = 0.11
+socso_deduction = 0.005
 
 if tax_status == "Single":
 
-    if net_salary_before_tax >= 5000:
+    if total_income >= 5000:
         tax_rate = 0.22
     else:
         tax_rate = 0.18
 
 elif tax_status == "Married":
 
-    if net_salary_before_tax >= 6000:
+    if total_income >= 6000:
         tax_rate = 0.20
     else:
         tax_rate = 0.15
 
 elif tax_status == "Head":
 
-    if net_salary_before_tax >= 5500:
+    if total_income >= 5500:
         tax_rate = 0.25
     else:
         tax_rate = 0.19
 
-net_salary = net_salary_before_tax * (1 - tax_rate)
+net_salary = total_income * (1 - tax_rate) * (1 - (epf_deduction + socso_deduction))
 tax_rate = f"{int(tax_rate * 100)}%"
 
 print(employee_name)
--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -4,6 +4,47 @@ customer_type = input()
 is_student = input()
 
 # TODO your code here
+final_price = 0
+
+if show_time > 18:
+    final_price += 3
+
+    # Weekday pricing
+if day_type == "Weekday" and customer_type == "Adult" and is_student == "Yes":
+    base_price = 15.00
+    final_price = (final_price + base_price) * 0.9
+elif day_type == "Weekday" and customer_type == "Child" and is_student == "Yes":
+    base_price = 10.00
+    final_price = (final_price + base_price) * 0.9
+elif day_type == "Weekday" and customer_type == "Senior" and is_student == "Yes":
+    base_price = 12.00
+    final_price = (final_price + base_price) * 0.9
+elif day_type == "Weekday" and customer_type == "Adult" and is_student == "No":
+    base_price = 15.00
+    final_price = (final_price + base_price)
+elif day_type == "Weekday" and customer_type == "Child" and is_student == "No":
+    base_price = 10.00
+    final_price = (final_price + base_price)
+elif day_type == "Weekday" and customer_type == "Senior" and is_student == "No":
+    base_price = 12.00
+    final_price = (final_price + base_price)
+    final_price = base_price
+
+    # Weekend pricing
+elif day_type == "Weekend" and customer_type == "Adult":
+    base_price = 18.00
+    final_price = (final_price + base_price)
+elif day_type == "Weekend" and customer_type == "Child":
+    base_price = 12.00
+    final_price = (final_price + base_price)
+elif day_type == "Weekend" and customer_type == "Senior":
+    base_price = 15.00
+    final_price = (final_price + base_price)
+
+
+
+
+
 
 print(base_price)
 print(final_price)
