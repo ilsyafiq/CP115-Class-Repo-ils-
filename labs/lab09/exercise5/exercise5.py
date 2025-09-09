569ac9ee1f1b91f587ed7593b1e1e97053103262
ilsyafiq <syfiqiikhwan@gmail.com>
2025-09-09 12:09:31 +0800

    Exercise 6

--- labs/lab09/exercise5/exercise5.py
+++ labs/lab09/exercise5/exercise5.py
@@ -5,5 +5,84 @@ customer_age = int(input())
 
 # TODO: Your code here
 
+# Chicken
+if main_course == "Chicken":
+    total_food_cost = 10.00
+
+    if drink == "Soft Drink":
+        total_food_cost += 2.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+
+
+    elif drink == "Coffee":
+        total_food_cost += 3.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+        
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+
+# Beef
+elif main_course == "Beef":
+    total_food_cost = 12.00
+
+    if drink == "Soft Drink":
+        total_food_cost += 2.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+    elif drink == "Coffee":
+        total_food_cost += 3.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+
+# Fish
+elif main_course == "Fish":
+    total_food_cost = 11.00
+
+    if drink == "Soft Drink":
+        total_food_cost += 2.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+    elif drink == "Coffee":
+        total_food_cost += 3.00
+
+        if dessert == "Ice Cream":
+            total_food_cost += 4.00
+
+        elif dessert == "Cake":
+            total_food_cost += 5.00
+
+
+if customer_age < 18:
+    final_bill = total_food_cost * 1.1 * 0.9  # 10% service charge and 10% discount
+
+elif customer_age > 60:
+    final_bill = total_food_cost * 1.1 * 0.85 # 10% service charge and 15% discount
+
+else:
+    final_bill = total_food_cost * 1.1        # 10% service charge
 
 print(f"{final_bill:.2f}")
--- labs/lab09/exercise6/exercise6.py
+++ labs/lab09/exercise6/exercise6.py
@@ -2,7 +2,24 @@ position = input()
 overtime_hours = int(input())
 is_weekend = input()
 
-# TODO: Your code here
+if position == "Manager":
+    base_rate = 30
+elif position == "Supervisor":
+    base_rate = 20
+elif position == "Staff":
+    base_rate = 15
+elif position == "Intern":
+    base_rate = 8
+
+if overtime_hours == 0:
+    overtime_pay = 0
+elif overtime_hours <= 8:
+    overtime_pay = overtime_hours * base_rate * 1.5
+else:
+    overtime_pay = (8 * base_rate * 1.5) + ((overtime_hours - 8) * base_rate * 2.0)
+
+if is_weekend == "Yes":
+    overtime_pay += overtime_hours * 5
 
 
 print(overtime_pay)
